aafec62893a29e1786962de0ba4655085a5f564e
Cherry Jane A. lora <qwerty_q000@yahoo.com>
2014-08-14 11:26:58 +0800

    initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webtech::Application.config.secret_key_base = '2d5d77c537cc9c55751b5a77f500a94b4f6bb0ebd4ae1e7f7753e0ce18b2f0c1424438c1acfab635a7d58da0862a0b3d52b3fe20a821c3d8bb6851409e0ca2eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webtech::Application.config.session_store :cookie_store, key: '_webtech_session'
